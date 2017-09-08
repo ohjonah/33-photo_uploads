c5290bf2c40526d0cbedce224872c94952f73ddf
ohjonah <ohjonah@gmail.com>
2017-09-08 07:43:48 -0700

    TS Loading

--- frontend/src/lib/util.js
+++ frontend/src/lib/util.js
@@ -1,3 +1,32 @@
+export const photoToDataURL = (file) => {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    reader.addEventListener('load', () => {
+      resolve(reader.result);
+    })
+    reader.addEventListener('error', () => {
+      reject(reader.error);
+    });
+    if (file) {
+      return reader.readAsDataURL(file)
+    }
+    return reject(new Error('USAGE ERROR: requires file'));
+  })
+}
+
+// got from:
+// https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
+export const readCookie = (name) => {
+  var nameEQ = name + '=';
+  var ca = document.cookie.split(';');
+  for(var i=0; i < ca.length; i++) {
+    var c = ca[i];
+    while(c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
 export const log = (...args) =>
   __DEBUG__ ? console.log(...args) : undefined;
 
