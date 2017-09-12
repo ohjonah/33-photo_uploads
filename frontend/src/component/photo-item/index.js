0b409d95c411cf0ed0f1be3cf4222c9d1216afa4
ohjonah <ohjonah@gmail.com>
2017-09-11 18:45:54 -0700

    Adds Photo Item

---
+++ frontend/src/component/photo-item/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import PhotoForm from '../photo-form';
+import * as util from '../../lib/util.js';
+import * as photoActions from '../../action/photo-actions.js';
+
+export class PhotoItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editing: false
+    }
+
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+  }
+
+  handleDelete() {
+    return this.props.deletePhoto(this.props.photo)
+    .then(console.log)
+    .catch(console.error)
+  }
+
+  handleUpdate(photo) {
+    return this.props.updatePhoto(photo)
+    .then(() => {
+      this.setState({
+        editing: false
+      })
+      .catch(console.error)
+    })
+  }
+
+  render() {
+    let {photo} = this.props;
+
+    return (
+      <div>
+        {util.renderIf(!this.state.editing,
+          <div>
+            <img src={photo.url} />
+            <p>{photo.description}</p>
+            <button type='submit' onClick={this.handleDelete}>delete</button>
+            <button type='submit' onClick={() => this.setState({
+              editing: true
+            })}>delete</button>
+          </div>
+        )}
+
+        {util.render(this.state.editing,
+          <div>
+            <PhotoForm
+              photo={this.props.photo}
+              buttonText='update photo'
+              onComplete={this.handleUpdate}
+              />
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+let mapStateToProps = () => ({});
+
+let mapDispatchToProps = (dispatch) => ({
+  deletePhoto: (photo) => dispatch(photoActions.deleteUserPhotoRequest(photo)),
+  updatePhoto: (photo) => dispatch(photoActions.updateUserPhotoRequest(photo)),
+})
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(PhotoItem)
